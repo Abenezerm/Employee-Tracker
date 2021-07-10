801c47c0969ad584f2b47591d37883cb6cc18d36
Abenezer Merdassa <abenezermerdassa@gmail.com>
2021-07-10 17:33:31 -0500

    set up connection to the db

---
+++ index.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+//set up connection to the mysql...
+const connection = mysql.createConnection({
+  host:'localhost',
+  port:3306,
+  user:'root',
+  password:'',
+  database: 'employees_db'
+});
+
+//
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`connection as id ${connection.threadId}\n`);
+});
